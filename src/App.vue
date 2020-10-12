481fa7c002a1d04ef3d3f104b8f7fbe37633717c
Olivier Mairet <oliviermairet@gmail.com>
2020-10-12 16:15:37 +0200

    add axios to call api

--- src/App.vue
+++ src/App.vue
@@ -1,67 +1,29 @@
 <template>
   <div id="app">
-    <md-card>
-      <md-card-header>
-        <div class="md-title">Material Design with Vue.js 2 Demo</div>
-        <div class="md-subhead">by CodingTheSmartWay.com</div>
-      </md-card-header>
-      <md-card-content>
-        <md-button class="md-raised md-primary" v-on:click="fillTable()">Fill table</md-button>
-        <md-button class="md-raised md-primary" v-on:click="clearTable()">Clear table</md-button>
-      </md-card-content>
-    </md-card>
-    
-    <br />
-
-    <md-table v-model="users" md-card>
-      <md-table-toolbar>
-        <h1 class="md-title">Users</h1>
-      </md-table-toolbar>
-
-      <md-table-row>
-        <md-table-head md-numeric>ID</md-table-head>
-        <md-table-head>Name</md-table-head>
-        <md-table-head>Email</md-table-head>
-        <md-table-head>Gender</md-table-head>
-        <md-table-head>Job Title</md-table-head>
-      </md-table-row>
-
-      <md-table-row slot="md-table-row">
-        <md-table-cell md-label="ID" md-sort-by="id" md-numeric>test</md-table-cell>
-        <md-table-cell md-label="Name" md-sort-by="name">test</md-table-cell>
-        <md-table-cell md-label="Email" md-sort-by="email">test</md-table-cell>
-        <md-table-cell md-label="Gender" md-sort-by="gender">test</md-table-cell>
-        <md-table-cell md-label="Job Title" md-sort-by="title">test</md-table-cell>
-      </md-table-row>
-      <md-table-row slot="md-table-row">
-        <md-table-cell md-label="ID" md-sort-by="id" md-numeric>test</md-table-cell>
-        <md-table-cell md-label="Name" md-sort-by="name">test</md-table-cell>
-        <md-table-cell md-label="Email" md-sort-by="email">test</md-table-cell>
-        <md-table-cell md-label="Gender" md-sort-by="gender">test</md-table-cell>
-        <md-table-cell md-label="Job Title" md-sort-by="title">test</md-table-cell>
-      </md-table-row>
-      <md-table-row slot="md-table-row">
-        <md-table-cell md-label="ID" md-sort-by="id" md-numeric>test</md-table-cell>
-        <md-table-cell md-label="Name" md-sort-by="name">test</md-table-cell>
-        <md-table-cell md-label="Email" md-sort-by="email">test</md-table-cell>
-        <md-table-cell md-label="Gender" md-sort-by="gender">test</md-table-cell>
-        <md-table-cell md-label="Job Title" md-sort-by="title">test</md-table-cell>
-      </md-table-row>
-      <md-table-row slot="md-table-row">
-        <md-table-cell md-label="ID" md-sort-by="id" md-numeric>test</md-table-cell>
-        <md-table-cell md-label="Name" md-sort-by="name">test</md-table-cell>
-        <md-table-cell md-label="Email" md-sort-by="email">test</md-table-cell>
-        <md-table-cell md-label="Gender" md-sort-by="gender">test</md-table-cell>
-        <md-table-cell md-label="Job Title" md-sort-by="title">test</md-table-cell>
-      </md-table-row>
-    </md-table>
+    <Recipe v-bind:recipes="apiResults" />
   </div>
 </template>
 
 <script>
+import Recipe from './components/Recipe.vue'
+import axios from 'axios';
+
 export default {
   name: 'App',
-  components: {}
+  components: {
+    Recipe
+  },
+  data () {
+    return {
+      apiResults: null
+    }
+  },
+  mounted () {
+    axios
+      .get('https://127.0.0.1:8000/api/recipes')
+      .then(response => (this.apiResults = response.data['hydra:member']))
+      
+  }
 }
 </script>
 
--- src/components/Recipe.vue
+++ src/components/Recipe.vue
@@ -1,41 +1,41 @@
 <template>
-  <div id="app">
-    <md-card>
-      <md-card-header>
-        <div class="md-title">Material Design with Vue.js 2 Demo</div>
-        <div class="md-subhead">by CodingTheSmartWay.com</div>
-      </md-card-header>
-      <md-card-content>
-        
-        <md-button class="md-raised md-primary" v-on:click="fillTable()">Fill table</md-button>
-        <md-button class="md-raised md-primary" v-on:click="clearTable()">Clear table</md-button>
-        <br />
-         <md-table>
-          <md-table-header>
+    <div>
+        <md-card>
+            <md-card-header>
+                <div class="md-title">Material Design with Vue.js 2 Demo</div>
+                <div class="md-subhead">by CodingTheSmartWay.com</div>
+            </md-card-header>
+            <md-card-content>
+                <md-button class="md-raised md-primary" v-on:click="fillTable()">Fill table</md-button>
+                <md-button class="md-raised md-primary" v-on:click="clearTable()">Clear table</md-button>
+            </md-card-content>
+            </md-card>
+            
+            <br />
+            
+            <md-table md-card>
+            <md-table-toolbar>
+                <h1 class="md-title">Recipes</h1>
+            </md-table-toolbar>
+
             <md-table-row>
-              <md-table-head>First name</md-table-head>
-              <md-table-head>Last name</md-table-head>
-              <md-table-head>Email</md-table-head>
+                <md-table-head md-numeric>ID</md-table-head>
+                <md-table-head>Name</md-table-head>
             </md-table-row>
-          </md-table-header>
-          <md-table-body>
-            <md-table-row>
-              <md-table-cell>test</md-table-cell>
-              <md-table-cell>test</md-table-cell>
-              <md-table-cell>test</md-table-cell>
+
+            <md-table-row slot="md-table-row" v-bind:key="recipe.id" v-for="recipe in recipes">
+                <md-table-cell md-label="ID" md-sort-by="id" md-numeric>{{recipe.id}}</md-table-cell>
+                <md-table-cell md-label="Name" md-sort-by="name">{{recipe.name}}</md-table-cell>
             </md-table-row>
-          </md-table-body>
         </md-table>
-      </md-card-content>
-    </md-card>
-  </div>
+    </div>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
+  name: 'Recipe',
   props: {
-    msg: String
+    recipes: Array
   }
 }
 </script>

e1433124dfd4caa8fd9edf29af87dd6f7247b40e
Olivier Mairet <oliviermairet@gmail.com>
2020-10-12 10:12:08 +0200

    using vue-cli and material design to generate proper app

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,72 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <md-card>
+      <md-card-header>
+        <div class="md-title">Material Design with Vue.js 2 Demo</div>
+        <div class="md-subhead">by CodingTheSmartWay.com</div>
+      </md-card-header>
+      <md-card-content>
+        <md-button class="md-raised md-primary" v-on:click="fillTable()">Fill table</md-button>
+        <md-button class="md-raised md-primary" v-on:click="clearTable()">Clear table</md-button>
+      </md-card-content>
+    </md-card>
+    
+    <br />
+
+    <md-table v-model="users" md-card>
+      <md-table-toolbar>
+        <h1 class="md-title">Users</h1>
+      </md-table-toolbar>
+
+      <md-table-row>
+        <md-table-head md-numeric>ID</md-table-head>
+        <md-table-head>Name</md-table-head>
+        <md-table-head>Email</md-table-head>
+        <md-table-head>Gender</md-table-head>
+        <md-table-head>Job Title</md-table-head>
+      </md-table-row>
+
+      <md-table-row slot="md-table-row">
+        <md-table-cell md-label="ID" md-sort-by="id" md-numeric>test</md-table-cell>
+        <md-table-cell md-label="Name" md-sort-by="name">test</md-table-cell>
+        <md-table-cell md-label="Email" md-sort-by="email">test</md-table-cell>
+        <md-table-cell md-label="Gender" md-sort-by="gender">test</md-table-cell>
+        <md-table-cell md-label="Job Title" md-sort-by="title">test</md-table-cell>
+      </md-table-row>
+      <md-table-row slot="md-table-row">
+        <md-table-cell md-label="ID" md-sort-by="id" md-numeric>test</md-table-cell>
+        <md-table-cell md-label="Name" md-sort-by="name">test</md-table-cell>
+        <md-table-cell md-label="Email" md-sort-by="email">test</md-table-cell>
+        <md-table-cell md-label="Gender" md-sort-by="gender">test</md-table-cell>
+        <md-table-cell md-label="Job Title" md-sort-by="title">test</md-table-cell>
+      </md-table-row>
+      <md-table-row slot="md-table-row">
+        <md-table-cell md-label="ID" md-sort-by="id" md-numeric>test</md-table-cell>
+        <md-table-cell md-label="Name" md-sort-by="name">test</md-table-cell>
+        <md-table-cell md-label="Email" md-sort-by="email">test</md-table-cell>
+        <md-table-cell md-label="Gender" md-sort-by="gender">test</md-table-cell>
+        <md-table-cell md-label="Job Title" md-sort-by="title">test</md-table-cell>
+      </md-table-row>
+      <md-table-row slot="md-table-row">
+        <md-table-cell md-label="ID" md-sort-by="id" md-numeric>test</md-table-cell>
+        <md-table-cell md-label="Name" md-sort-by="name">test</md-table-cell>
+        <md-table-cell md-label="Email" md-sort-by="email">test</md-table-cell>
+        <md-table-cell md-label="Gender" md-sort-by="gender">test</md-table-cell>
+        <md-table-cell md-label="Job Title" md-sort-by="title">test</md-table-cell>
+      </md-table-row>
+    </md-table>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
+  components: {}
 }
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  padding: 50px;
 }
 </style>
---
+++ src/components/Recipe.vue
@@ -0,0 +1,41 @@
+<template>
+  <div id="app">
+    <md-card>
+      <md-card-header>
+        <div class="md-title">Material Design with Vue.js 2 Demo</div>
+        <div class="md-subhead">by CodingTheSmartWay.com</div>
+      </md-card-header>
+      <md-card-content>
+        
+        <md-button class="md-raised md-primary" v-on:click="fillTable()">Fill table</md-button>
+        <md-button class="md-raised md-primary" v-on:click="clearTable()">Clear table</md-button>
+        <br />
+         <md-table>
+          <md-table-header>
+            <md-table-row>
+              <md-table-head>First name</md-table-head>
+              <md-table-head>Last name</md-table-head>
+              <md-table-head>Email</md-table-head>
+            </md-table-row>
+          </md-table-header>
+          <md-table-body>
+            <md-table-row>
+              <md-table-cell>test</md-table-cell>
+              <md-table-cell>test</md-table-cell>
+              <md-table-cell>test</md-table-cell>
+            </md-table-row>
+          </md-table-body>
+        </md-table>
+      </md-card-content>
+    </md-card>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  }
+}
+</script>
